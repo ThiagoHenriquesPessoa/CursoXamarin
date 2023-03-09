f81a08320e1560ed567ff5f8e362e40d0da71773
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-03-08 22:58:21 -0300

    Add project files.

---
+++ CursoXamarin/CursoXamarin/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace CursoXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
