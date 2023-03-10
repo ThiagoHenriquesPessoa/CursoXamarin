bb88c004921e5d0dbb40ffd78782e9c7b25db7cf
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-03-09 23:39:06 -0300

    implementando uma calculadora

--- CursoXamarin/CursoXamarin/App.xaml.cs
+++ CursoXamarin/CursoXamarin/App.xaml.cs
@@ -8,7 +8,7 @@ namespace CursoXamarin
         {
             InitializeComponent();
 
-            MainPage = new ParOuImpar();
+            MainPage = new CalculadoraSimples();
         }
 
         protected override void OnStart()
---
+++ CursoXamarin/CursoXamarin/CalculadoraSimples.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CursoXamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalculadoraSimples : ContentPage
+    {
+        public CalculadoraSimples()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecutar_Clicked(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+            int result;
+            if (bt.Text == "+")
+            {
+                result = Convert.ToInt32(etValorA.Text) + Convert.ToInt32(etValorB.Text);
+            }
+            else if (bt.Text == "-") 
+            {
+                result = Convert.ToInt32(etValorA.Text) - Convert.ToInt32(etValorB.Text);
+            }
+            else if(bt.Text == "*") 
+            {
+                result = Convert.ToInt32(etValorA.Text) * Convert.ToInt32(etValorB.Text);
+            }
+            else
+            {
+                result = Convert.ToInt32(etValorA.Text) / Convert.ToInt32(etValorB.Text);
+            }
+            lbResposta.Text = Convert.ToString(result);
+        }       
+    }
+}
